6631b154821f557db207d4dc4ac0194fbead51f2
Julian Simioni <julian.simioni@gmail.com>
2013-02-01 18:30:07 -0800

    Initial commit

---
+++ problem_3.py
@@ -0,0 +1,65 @@
+# 3. Write a function that takes an arithmetic expression in string form (e.g. "5 x 6 / 3 + 1") and returns the 
+#    numerical result. Assume the arithmetic expression only consists of DMAS (div/mul/add/sub) operations.
+
+def is_operator(token):
+    return token in ['+', '-', 'x', '/']
+
+def precedence(token):
+    if token in ['+', '-']:
+        return 1
+    return 2
+
+def do_operator(operator, operand1, operand2):
+    if operator == '+':
+        return operand1 + operand2
+    elif operator == '-':
+        return operand1 - operand2
+    elif operator == 'x':
+        return operand1 * operand2
+    elif operator == '/':
+        return operand1 / operand2
+
+def evaluate(input):
+    tokens = parse(input)
+
+    operator_stack = []
+    operand_stack = []
+
+    for token in tokens:
+        if is_operator(token):
+            operator_stack.append(token)
+        else:
+            operand_stack.append(token)
+
+
+    side_operator_stack = []
+    side_operand_stack = []
+    while operator_stack:
+        precedence_differs = len(operator_stack)> 1 and precedence(operator_stack[-1]) < precedence(operator_stack[-2])
+        if operator_stack and precedence_differs:
+            side_operator_stack.append(operator_stack.pop())
+            side_operand_stack.append(operand_stack.pop())
+            continue
+        elif side_operator_stack and precedence(operator_stack[-1]) <= precedence(side_operator_stack[-1]):
+            operator = side_operator_stack.pop()
+            operand1 = side_operand_stack.pop()
+            operand2 = operand_stack.pop()
+        else:
+            operator = operator_stack.pop()
+            operand1 = operand_stack.pop()
+            operand2 = operand_stack.pop()
+
+        result = do_operator(operator, operand1, operand2)
+        operand_stack.append(result)
+
+    return operand_stack.pop()
+
+def parse(input):
+    parts = input.split()
+    new_parts = []
+    for token in parts:
+        try:
+            new_parts.append(int(token))
+        except ValueError:
+            new_parts.append(token)
+    return new_parts
---
+++ test.py
@@ -0,0 +1,30 @@
+from nose.tools import *
+
+from problem_3 import *
+
+def test_add():
+    input = "1 + 1"
+    assert_equal(evaluate(input), 2)
+
+def test_parse():
+    input = "1 + 1"
+    assert_equal(parse(input), [1, '+', 1])
+
+def test_two_adds():
+    input = "1 + 1 + 2"
+    assert_equal(evaluate(input), 4)
+
+def test_add_multiply():
+    input = "3 x 2 + 1"
+    assert_equal(evaluate(input), 7)
+
+def test_add_multiply2():
+    input = "1 + 2 x 3"
+    assert_equal(evaluate(input), 7)
+
+def test_precedence():
+    operators = [ '+', '-', 'x', '/' ]
+    precedence_val = [1, 1, 2, 2]
+
+    for idx, operator in enumerate(operators):
+        assert_equal(precedence(operator), precedence_val[idx])
