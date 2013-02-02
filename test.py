b6eb7538156556f18a765f24200f16093d1168d4
Julian Simioni <julian.simioni@gmail.com>
2013-02-02 00:28:42 -0800

    Test two versions of the evaluate function

--- test.py
+++ test.py
@@ -2,9 +2,15 @@ from nose.tools import *
 
 from problem_3 import *
 
+functions = [evaluate, evaluate2]
+
+def _test_both(input, expected):
+    for f in functions:
+        assert_equal(f(input), expected)
+
 def test_add():
     input = "1 + 1"
-    assert_equal(evaluate(input), 2)
+    _test_both(input, 2)
 
 def test_parse():
     input = "1 + 1"
@@ -12,35 +18,35 @@ def test_parse():
 
 def test_two_adds():
     input = "1 + 1 + 2"
-    assert_equal(evaluate(input), 4)
+    _test_both(input, 4)
 
 def test_add_multiply():
     input = "3 * 2 + 1"
-    assert_equal(evaluate(input), 7)
+    _test_both(input, 7)
 
 def test_add_multiply2():
     input = "1 + 2 * 3"
-    assert_equal(evaluate(input), 7)
+    _test_both(input, 7)
 
 def test_example():
     input = "5 * 6 / 3 + 1"
-    assert_equal(evaluate(input), 11)
+    _test_both(input, 11)
 
 def test_4_operators():
     input = "1 + 2 * 3 - 4 / 5"
-    assert_equal(evaluate(input), 6.2)
+    _test_both(input, 6.2)
 
 def test_power():
     input = "3 ^ 2"
-    assert_equal(evaluate(input), 9)
+    _test_both(input, 9)
 
 def test_pow_and_sub():
     input = "2 ^ 8 - 1"
-    assert_equal(evaluate(input), 255)
+    _test_both(input, 255)
 
 def test_pow_mult_sub():
     input = "2 ^ 8 * 4 - 1"
-    assert_equal(evaluate(input), 1023)
+    _test_both(input, 1023)
 
 def test_precedence():
     precedence_val = [1, 1, 2, 2, 3]
